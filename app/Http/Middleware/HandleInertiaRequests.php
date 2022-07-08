1ebd3cc2cd254ba6a6a520166b55a839d96ad3a4
Shakil Alam <itxshakil@gmail.com>
2022-07-08 11:34:15 +0530

    Send current admin with request share data

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,6 +37,7 @@ class HandleInertiaRequests extends Middleware
         return array_merge(parent::share($request), [
             'auth' => [
                 'user' => $request->user(),
+                'admin' => $request->user('admin'),
             ],
             'ziggy' => function () use ($request) {
                 return array_merge((new Ziggy)->toArray(), [
