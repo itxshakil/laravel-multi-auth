52e9d5cf077e801b697281d901a895dbe8922bdf
Shakil Alam <itxshakil@gmail.com>
2022-07-08 09:19:15 +0530

    Add Admin model, migration, factory and setup seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // \App\Models\Admin::factory()->create([
+        //     'name' => 'Test Admin',
+        //     'email' => 'admin@example.com',
+        // ]);
     }
 }
