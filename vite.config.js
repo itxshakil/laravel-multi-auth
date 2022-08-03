116dd734779c24065e549ecaf766aea524998ef0
Shakil Alam <itxshakil@gmail.com>
2022-08-03 21:20:16 +0530

    Switch to fonts.bunny.net instead of Google Fonts

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
-            refresh: true
+            input: "resources/js/app.js",
+            refresh: true,
         }),
         vue({
             template: {
